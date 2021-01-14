b03d8d5c9234c32d2870f6e3ba7add079ac223ff
Rob <roborgain@gmail.com>
2021-01-13 22:54:26 -0800

    updated ipynb, created csv foler, created url folder, created schema folder

---
+++ sql_schema/league_win_rates.sql
@@ -0,0 +1,14 @@
+DROP TABLE league_winrates
+
+CREATE TABLE league_winrates (
+id INT PRIMARY KEY,
+rank VARCHAR(30),
+champion VARCHAR(30),
+role VARCHAR(30),
+win_Rate VARCHAR(30)
+);
+
+SELECT * FROM league_winrates
+
+SELECT * FROM league_winrates
+WHERE champion = 'Rumble'
---
+++ website_urls/urls.py
@@ -0,0 +1,5 @@
+platinum_scraped_url = 'https://champion.gg/statistics/overview?queue=ranked-solo-duo&rank=platinum_plus&region=world'
+
+master_scraped_url = 'https://champion.gg/statistics/overview?queue=ranked-solo-duo&rank=master&region=world'
+
+platinum_splintered_url = 'https://www.leagueofgraphs.com/champions/builds/iron/sr-ranked'
